7a2ece3084010c882cc5b106bce6ef6ddbda6e26
Mohamed B <caulosegnin@gmail.com>
2022-10-27 18:12:48 +0200

    layout bearbeitung

--- php/dashboard.php
+++ php/dashboard.php
@@ -16,6 +16,10 @@ Dashboard
     <input type="submit" name="archiv" value=' Archiv einsehen' id='display'>
 </form>
 
+<form action="dashboard.php" method=POST>
+    <input type="submit" name="termin_erstellen" value=' Termin erstellen' id='display'>
+</form>
+
 
 <?php
 session_start();
@@ -38,10 +42,9 @@ $data_email = $data['email'];
 echo '<br>Willkommen ' . $data_vname . '!';
 
 ?>
-<br>
-Erstellen Sie hier schnell und zuverlässig einen Termin!
-<br>
-<form action="" method=POST>
+
+
+<!-- <form action="" method=POST>
     <label for="datum">Datum:</label>
     <input type="date" id="datum" name="datum" min="<?php echo date('Y-m-d'); ?>" value="<?php echo date('Y-m-d'); ?>" required>
 
@@ -51,13 +54,35 @@ Erstellen Sie hier schnell und zuverlässig einen Termin!
     <p><label for="temin">Terminbeschreibung:</label></p>
     <textarea id="termin" name="termin" rows="4" cols="50" placeholder="Termin hier eintragen...max 100 Zeichen" maxlength='100' required></textarea>
     <input type="submit" name="termin_submit" value=' Termin bestätigen'>
-</form>
+</form> -->
 
 
 
 <?php
 
-//Löschen
+if (isset($_POST['termin_erstellen'])) {
+
+    $val_input = date('Y-m-d');
+    $val_inputzeit = date("h:i");
+
+
+    echo '
+    <p>Erstellen Sie hier schnell und zuverlässig einen Termin!</p>
+<form action="" method=POST>
+<label for="datum">Datum:</label>
+<input type="date" id="datum" name="datum" min="' . $val_input . '" value="' . $val_input . '" required>
+
+<label for="uhrzeit">Uhrzeit:</label>
+<input type="time" id="uhrzeit" name="uhrzeit" value="' . $val_inputzeit . '" required>
+
+<p><label for="temin">Terminbeschreibung:</label></p>
+<textarea id="termin" name="termin" rows="4" cols="50" placeholder="Termin hier eintragen...max 100 Zeichen" maxlength="100" required></textarea>
+<input type="submit" name="termin_submit" value=" Termin bestätigen">
+</form>
+
+';
+}
+
 if (isset($_GET["loeschen"])) {
 
     $befehl_termin_loechen = "SELECT termin, datum, uhrzeit FROM eintraege WHERE  termin_pk =" . $_GET["loeschen"];
@@ -73,10 +98,10 @@ if (isset($_GET["loeschen"])) {
     echo '<p>Der Termin am ' . $datadate_loechen . ' um ' .  $datazeit_loechen . ' wurde erfolgreich gelöscht! </p>';
     echo '<p>Inhalt der Terminbeschreibung: ' . $datatermin_loechen . '</p>';
     mysqli_close($link);
-    // die;
 }
 
 if (isset($_POST['termin_submit'])) {
+
     $datum = htmlspecialchars($_POST['datum']);
     $uhrzeit = htmlspecialchars($_POST['uhrzeit']);
     $termin = mysqli_real_escape_string($link, $_POST['termin']);
@@ -88,44 +113,6 @@ if (isset($_POST['termin_submit'])) {
 
     $befehl_termin = "INSERT INTO eintraege(termin_fk, termin, datum, uhrzeit) VALUES ('$session_pk','$termin','$datum','$uhrzeit')";
     $antwort_termin = mysqli_query($link, $befehl_termin);
-
-    /*
-    //Auflistung aller Einträge.
-    $befehl_terminanzeige = "SELECT termin, datum, uhrzeit FROM eintraege ORDER BY datum";
-    $antwort_terminanzeige = mysqli_query($link, $befehl_terminanzeige);
-    echo "
-        <table>
-            <tr>
-                <td> Löschen </td>
-                <td> termin </td>
-                <td> datum </td>
-                <td> uhrzeit </td>
-            </tr>
-        ";
-    // $i = 0;
-    //Anzeige zukünftiger Termine.
-    while ($data_terminanzeige = mysqli_fetch_array($antwort_terminanzeige)) {
-        // $i++;
-        // $datafk = $data_terminanzeige['termin_fk'];
-        $datatermin = $data_terminanzeige['termin'];
-        $datadate = $data_terminanzeige['datum'];
-        // $datazeit = $data_terminanzeige['uhrzeit'];
-        $datazeit = substr($data_terminanzeige['uhrzeit'], 0, 5);
-
-        if ($datadate >= date('Y-m-d')) {
-            echo "
-            <tr>
-                <td><a href='?loeschen=" . $datatermin . "'>Löschen</a></td>
-                <td> " . $datatermin . " </td>
-                <td> " . $datadate . " </td>
-                <td> " . $datazeit . "  </td>
-            </tr>
-            ";
-        }
-    }
-    echo " 
-        </table>";
-    */
     mysqli_close($link);
 }
 
@@ -134,27 +121,22 @@ if (isset($_POST['termin_submit'])) {
 
 if (isset($_POST['anzeige'])) {
 
-
-    //Auflistung aller Einträge.
     $befehl_terminanzeige = "SELECT termin, datum, uhrzeit, termin_pk FROM eintraege WHERE termin_fk= '$session_pk' ORDER BY datum";
     $antwort_terminanzeige = mysqli_query($link, $befehl_terminanzeige);
     echo "
     <table>
         <tr>
-            <td> Löschen </td>
-            <td> termin </td>
-            <td> datum </td>
-            <td> uhrzeit </td>
+            <td> Termin löschen </td>
+            <td> Beschreibung </td>
+            <td> Datum </td>
+            <td> Uhrzeit </td>
         </tr>
     ";
-    // $i = 0;
-    //Anzeige zukünftiger Termine.
     while ($data_terminanzeige = mysqli_fetch_array($antwort_terminanzeige)) {
-        // $i++;
+
         $datapk = $data_terminanzeige['termin_pk'];
         $datatermin = $data_terminanzeige['termin'];
         $datadate = $data_terminanzeige['datum'];
-        // $datazeit = $data_terminanzeige['uhrzeit'];
         $datazeit = substr($data_terminanzeige['uhrzeit'], 0, 5);
 
         if ($datadate >= date('Y-m-d')) {
@@ -189,7 +171,6 @@ if (isset($_POST['archiv'])) {
         </tr>
     ";
     $i = 0;
-    //Anzeige archivierter Termine.
     while ($data_terminanzeige = mysqli_fetch_array($antwort_terminanzeige)) {
 
         $datapk = $data_terminanzeige['termin_pk'];
@@ -210,7 +191,8 @@ if (isset($_POST['archiv'])) {
         }
 
         if ($i == 0) {
-            echo 'Keine archivierten Termine.';
+            echo '<p>Keine archivierten Termine.</p>';
+            // echo '<table id= eintraege_tab style="display:none;" > ';
             die;
         }
     }
@@ -218,4 +200,3 @@ if (isset($_POST['archiv'])) {
     </table>";
     mysqli_close($link);
 }
-
